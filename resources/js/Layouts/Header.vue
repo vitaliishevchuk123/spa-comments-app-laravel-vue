1f32c22bbf76d8a3be26f9bc5256638b441e9387
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2024-01-25 19:51:30 +0200

    install vue and set main layout

---
+++ resources/js/Layouts/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+
+    </header>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<template>
+    <div class="main-wrapper min-h-screen bg-gray-100">
+        <Header/>
+
+        <!-- Page Content -->
+        <main>
+            <slot/>
+        </main>
+        <Footer/>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.main-wrapper {
+    display: grid;
+    grid-template-rows: auto 1fr auto; /* автоматична висота хедера та футера, решта простір на контент */
+    min-height: 100vh; /* забезпечення повноекранного розміщення */
+}
+</style>
